ee5df204cfc42bb5da254179423ed920387cdb9e
ts <tristansetha@gmail.com>
2025-01-25 13:35:41 -0800

    update slider opacity

--- MovieAnalytics.Client/src/views/Home.tsx
+++ MovieAnalytics.Client/src/views/Home.tsx
@@ -20,14 +20,6 @@ export default function Home() {
       <SlidingBackground images={movieImages} interval={5000} />
 
       <div className="hero">
-        {/* <div className="floating-elements">
-        <span className="float-item">🎬</span>
-        <span className="float-item">🎥</span>
-        <span className="float-item">🍿</span>
-        <span className="float-item">🎭</span>
-        <span className="float-item">📽️</span>
-      </div> */}
-
         <div className="hero-content">
           <h1>Movie Analytics</h1>
           <p>Discover insights from over 33,000 movies</p>
@@ -36,10 +28,6 @@ export default function Home() {
           </button>
         </div>
         <div>
-          {/* <div style={{ position: 'relative', zIndex: 1, color: 'white', textAlign: 'center' }}>
-          <h1>Welcome to Movie Analytics</h1>
-          <p>Your personalized dashboard for movie trends and insights.</p>
-        </div> */}
         </div>
       </div>
     </>
